a78c93cea88a45e967a2ef8c0399dadff1fedda6
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-09-01 22:33:47 -0300

    Primeiros testes com Django

---
+++ Django/contas/form.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Transacao
+
+class TransacaoForm(ModelForm):
+    class Meta:
+        model = Transacao
+        fields = ['data', 'descricao', 'valor', 'categoria', 'observacoes']
---
+++ Django/marcar_consulta/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for marcar_consulta project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marcar_consulta.settings')
+
+application = get_asgi_application()
