fa63b40548659f0b1ea756d9b43244650aaf2623
Eduardo Almeida <eduardoeandroid@gmail.com>
2020-10-06 15:27:43 -0300

    Atualizacao

---
+++ Django/Avancado/apps/documentos/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+
+from apps.funcionarios.models import Funcionario
+
+
+class Documento(models.Model):
+    descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+    arquivo = models.FileField(upload_to='documentos')
+
+    def get_absolute_url(self):
+        return reverse('update_funcionario', args=[self.pertence.id])
+
+    def __str__(self):
+        return self.descricao
---
+++ Django/Avancado/apps/empresas/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100, help_text='Nome da empresa')
+
+    def __str__(self):
+        return self.nome
+
+    def get_absolute_url(self):
+        return reverse('home')
