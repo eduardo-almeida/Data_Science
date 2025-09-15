f617b153c37d335c32dd4f996be2289b4448cf04
Eduardo Almeida <eduardoeandroid@gmail.com>
2025-09-15 20:42:48 -0300

    Novo projeto

--- LLM/Bootcamp/Projeto1/main.py
+++
@@ -1,7 +0,0 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema.messages import HumanMessage, SystemMessage
-
-from dotenv import load_dotenv
-import os
-
-print("11111111111111111")
--- LLM/Projetos/teste.py
+++ LLM/Projetos/teste.py
@@ -1,33 +1,27 @@
-print("ola mundo")
-
-import torch
 from langchain_huggingface import ChatHuggingFace
-from langchain_community.llms import HuggingFaceHub
-
-
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_community.llms import huggingface_hub
+from langchain_groq import ChatGroq
 from langchain_ollama import ChatOllama
-from langchain_openai import ChatOpenAI
-
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
-
+from langchain_core.prompts import ChatPromptTemplate 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = HuggingFaceHub(
-      repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
-      model_kwargs={
-          "temperature": 0.1,
-          "return_full_text": False,
-          "max_new_tokens": 512,
-          #"stop": ["<|eot_id|>"],
-          # demais parâmetros que desejar
-      }
-  )
+# Exemplo com Hugging Face
+llm = ChatOllama(
+    model="phi3",
+    temperature=0.1
+)
+
+
+
 
 system_prompt = "Você é um assistente prestativo e está respondendo perguntas gerais."
 user_prompt = "{input}"
 
+
+## llama 3
 token_s, token_e = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>", "<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
 
 prompt = ChatPromptTemplate.from_messages([
@@ -35,19 +29,11 @@ prompt = ChatPromptTemplate.from_messages([
     ("user", user_prompt + token_e)
 ])
 
-chain = prompt | llm
+# chain = prompt | llm
 
 input = "Explique para mim em até 1 parágrafo o conceito de redes neurais, de forma clara e objetiva"
 
 # res = chain.invoke({"input": input})
-# print(res)
-print("-----")
-
-llm = ChatOllama(
-    model="phi3",
-    temperature=0.1
-)
-
 chain3 = prompt | llm
 res = chain3.invoke({"input": input})
 print(res.content)
