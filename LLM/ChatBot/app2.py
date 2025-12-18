c9f35e94180fbdff6eca9cbf057f3f46ad29bf81
Eduardo Almeida <eduardoeandroid@gmail.com>
2025-12-18 11:17:21 -0300

    atualizando o Dockerfile

--- LLM/ChatBot/app2.py
+++ LLM/ChatBot/app2.py
@@ -4,6 +4,6 @@ app = FastAPI()
 
 @app.post('/webhook/')
 async def webhook(request: Request):
-    data = await request.json()
-    print (data)
+    # data = await request.json()
+    # print (data)
     return {'status': 'success'}
